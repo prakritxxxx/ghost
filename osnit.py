146e5002c1ef744f277449f8e872d0284b165807
prakritxxxx <prakritmaanshrestha@gmail.com>
2022-07-16 13:52:24 +0530

    update

--- osnit.py
+++ osnit.py
@@ -19,6 +19,17 @@ print(colorama.Fore.GREEN + '''
 made by cipher!
 enjoy it!
 if you use it pls mention the creator!
+
+help box
+|---------------------------------|
+|    [+] instagram   ---> -i      |
+|    [+] facebook    ---> -f      |
+|    [+] twitch      ---> -t      |
+|    [+] reddit      ---> -r      |
+|    [+] twitter     ---> -tw     |
+|    [+] printest    ---> -p      |
+|                                 |
+|---------------------------------|
 ''')
 
 
@@ -30,43 +41,26 @@ while True:
     
     var2 = input(colorama.Fore.GREEN + "Enter user: ")
 
-    if var1 == 'help':
-        print(colorama.Fore.GREEN + '''
-                 [+] instagram   ---> -i
-                 [+] facebook    ---> -f
-                 [+] twitch      ---> -t
-                 [+] reddit      ---> -r
-                 [+] twitter     ---> -tw
-                 [+] printest    ---> -p
-                 ''')
     if var1 == '-i':
         print(colorama.Fore.GREEN + f"https://www.instagram.com/{var2}/")
-    else:
-         print(colorama.Fore.GREEN + "[+] NO user found")
 
-    if var1 == '-f':
+
+    elif var1 == '-f':
         print(colorama.Fore.GREEN + f"https://www.facebook.com/{var2}")
 
-    else:
-         print(colorama.Fore.GREEN + "[+] NO user found")
-    if var1 == '-t':
+
+    elif var1 == '-t':
         print(colorama.Fore.GREEN + f"https://www.twitch.tv/{var2}")
 
-    else:
-         print(colorama.Fore.GREEN + "[+] NO user found")   
-    if var1 == '-r':
+    elif var1 == '-r':
         print(colorama.Fore.GREEN + f"https://www.reddit.com/user/{var2}/")
-    
-    else:
-         print(colorama.Fore.GREEN + "[+] NO user found")
 
-    if var1 == '-tw':
+
+    elif var1 == '-tw':
         print(colorama.Fore.GREEN + f"https://twitter.com/{var2}")
     
-    else:
-         print(colorama.Fore.GREEN + "[+] NO user found")
 
-    if var1 == '-p':
+    elif var1 == '-p':
         print(colorama.Fore.GREEN + f"https://in.pinterest.com/{var2}/")
     
     else:
